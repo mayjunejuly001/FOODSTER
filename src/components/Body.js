0c965fd88fdb70cfb553f2c553cac642598e477d
mayjunejuly001 <mayankvermacod@gmail.com>
2023-09-21 23:41:33 +0530

    ep 6 finally donw

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,55 +1,62 @@
-import resList from '../Utils/mockData'
 import RestroCard from './RestroCard'
-import { useState } from 'react'
-import resList from '../Utils/mockData'
-// import resList from '../Utils/mockData'
+import { useEffect, useState } from 'react'
+import { ShimmerUI } from './ShimmerUI.JS'
 
 const Body = () => {
   //local state variable-super powerful variable
-  const [listofRestaurants, setlistofRestaurants] = useState(resList)
+  const [listofRestaurants, setlistofRestaurants] = useState([])
+  const [filteredRestaurant, setFilteredRestaurant] = useState([])
 
-  // normal state variable
-  // let listofRestaurants1 = [
-  //   {
-  //     info: {
-  //       id: '9052',
-  //       name: 'Subway',
-  //       cloudinaryImageId: '1ace5fa65eff3e1223feb696c956b38b',
-  //       locality: 'Central Plaza, Kalina',
-  //       areaName: 'Santacruz East',
-  //       costForTwo: '₹350 for two',
-  //       cuisines: ['Salads', 'Snacks', 'Desserts', 'Beverages'],
-  //       avgRating: 3.8,
-  //     },
-  //   },
-  //   {
-  //     info: {
-  //       id: '32399',
-  //       name: "McDonald's",
-  //       cloudinaryImageId: 'ee5f8e06b300efc07c9fe3f4df40dfc4',
-  //       locality: 'Kalina Artista',
-  //       areaName: 'Santacruz East',
-  //       costForTwo: '₹400 for two',
-  //       cuisines: ['Burgers', 'Beverages', 'Cafe', 'Desserts'],
-  //       avgRating: 4.3,
-  //     },
-  //   },
-  //   {
-  //     info: {
-  //       id: '78036',
-  //       name: 'Burger King',
-  //       cloudinaryImageId: 'e33e1d3ba7d6b2bb0d45e1001b731fcf',
-  //       locality: 'Central Plaza, Kalina',
-  //       areaName: 'Santacruz East',
-  //       costForTwo: '₹350 for two',
-  //       cuisines: ['Burgers', 'American'],
-  //       avgRating: 4.1,
-  //     },
-  //   },
-  // ]
-  return (
+  const [searchText, setsearchText] = useState('')
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    )
+
+    const json = await data.json()
+
+    console.log(json)
+    setlistofRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    )
+    setFilteredRestaurant(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    )
+  }
+
+  return listofRestaurants.length === 0 ? (
+    <ShimmerUI />
+  ) : (
     <div className='body'>
       <div className='filter'>
+        <div className='search'>
+          <input
+            type='text'
+            className='search-box'
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value)
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listofRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              )
+
+              setFilteredRestaurant(filteredRestaurant)
+
+              console.log(searchText)
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className='filter-btn'
           onClick={() => {
@@ -64,7 +71,7 @@ const Body = () => {
         </button>
       </div>
       <div className='res-container'>
-        {listofRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestroCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import theImage from '../../logo.png'
+import { useState } from 'react'
 
 const Header = () => {
+  const [btnName, setbtnName] = useState('Login')
+
   return (
     <div className='header'>
       <div className='logo-container'>
@@ -12,6 +15,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className='btn'
+            onClick={() => {
+              btnName === 'Login' ? setbtnName('Logout') : setbtnName('Login')
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
